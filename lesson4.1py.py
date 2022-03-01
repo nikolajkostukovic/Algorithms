8951c0cefe7d572874a72757b9fc2c5dbaed76e4
Lucky_Days <84400532+nikolajkostukovic@users.noreply.github.com>
2022-03-01 16:50:15 +0300

    Add files via upload

---
+++ lesson4.1py.py
@@ -0,0 +1,24 @@
+#1
+'''
+Реализовать структуру данных стэк,
+который за О(1) выдает минимум в стэке:
+'''
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self,val:int) -> None:
+        if not self.stack:
+            self.stack.append(val,val)
+        else:
+            self.stack.append(val,min(val, self.stack[-1][1]))
+
+    def pop(self)-> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self)-> int:
+        return self.stack[-1][0]
+
+    def getMin(self)-> int:
+        return self.stack[-1][1]
---
+++ lesson4.2.py
@@ -0,0 +1,24 @@
+#2
+'''
+Обработка сетевых пакетов
+Реализовать обработчик сетевых пакетов.
+'''
+
+from collections import deque
+
+size, n = map(int,input().split())
+queue = deque
+
+for i in range(n):
+    arrival,duration = map(int,input().split())
+
+    while queue and queue[0] <= arrival:
+        queue.popleft()
+
+    if len(queue) < size:
+        if queue:
+            arrival = max(arrival,queue[-1])
+        print(arrival)
+        queue.append(arrival + duration)
+    else:
+        print(-1)
